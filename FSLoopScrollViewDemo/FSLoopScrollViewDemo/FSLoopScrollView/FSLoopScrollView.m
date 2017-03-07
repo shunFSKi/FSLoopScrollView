7e0c792ae6970aece02a7201a2d1c5d4d1dc1620
fengshun <fengshun@huim.com>
2017-03-07 18:39:36 +0800

    添加注释、区分横向纵向滚动、添加文字轮播

--- FSLoopScrollViewDemo/FSLoopScrollViewDemo/FSLoopScrollView/FSLoopScrollView.m
+++ FSLoopScrollViewDemo/FSLoopScrollViewDemo/FSLoopScrollView/FSLoopScrollView.m
@@ -9,15 +9,20 @@
 #import "FSLoopScrollView.h"
 #import "NSTimer+FSLoop.h"
 #import <UIImageView+WebCache.h>
+#import <YYText.h>
 
 @interface FSLoopScrollView ()<UIScrollViewDelegate>
 
 @property (nonatomic, strong)UIPageControl *pageControl;
 @property (nonatomic, strong) UIScrollView *scrollView;
-@property (nonatomic, strong) UIImageView *leftImgView;
-@property (nonatomic, strong) UIImageView *rightImgView;
+@property (nonatomic, strong) UIImageView *lastImgView;
+@property (nonatomic, strong) UIImageView *nextImgView;
 @property (nonatomic, strong) UIImageView *currentImgView;
+@property (nonatomic, strong) YYLabel *currentLabel;
+@property (nonatomic, strong) YYLabel *nextLabel;
 @property (nonatomic, strong) NSTimer *timer;
+@property (nonatomic, assign) BOOL isHorizontal;
+@property (nonatomic, assign) BOOL isTitleView;
 @end
 
 @implementation FSLoopScrollView
@@ -31,27 +36,24 @@
 */
 - (void)layoutSubviews
 {
-    _leftImgView.frame = CGRectMake(0, 0, CGRectGetWidth(self.scrollView.bounds), CGRectGetHeight(self.scrollView.bounds));
+    _lastImgView.frame = CGRectMake(0, 0, CGRectGetWidth(self.scrollView.bounds), CGRectGetHeight(self.scrollView.bounds));
+        
+    _currentImgView.frame = _isHorizontal?CGRectOffset(_lastImgView.frame, CGRectGetWidth(_lastImgView.bounds), 0):CGRectOffset(_lastImgView.frame, 0, CGRectGetHeight(_lastImgView.bounds));
+        
+    _nextImgView.frame = _isHorizontal?CGRectOffset(_currentImgView.frame, CGRectGetWidth(_currentImgView.bounds), 0):CGRectOffset(_currentImgView.frame, 0, CGRectGetHeight(_currentImgView.bounds));
     
-    _currentImgView.frame = CGRectOffset(_leftImgView.frame, CGRectGetWidth(_leftImgView.bounds), 0);
+    _currentLabel.frame = CGRectMake(0, 0, CGRectGetWidth(self.bounds), CGRectGetHeight(self.bounds));
     
-    _rightImgView.frame = CGRectOffset(_currentImgView.frame, CGRectGetWidth(_currentImgView.bounds), 0);
+    _nextLabel.frame = CGRectOffset(_currentLabel.frame, 0, CGRectGetHeight(_currentLabel.bounds));
 }
-
+    
 - (instancetype)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
     if (self) {
-        _leftImgView = [[UIImageView alloc]init];
-        [self.scrollView addSubview:_leftImgView];
-        
-        _rightImgView = [[UIImageView alloc]init];
-        [self.scrollView addSubview:_rightImgView];
-        
-        _currentImgView = [[UIImageView alloc]init];
-        [self.scrollView addSubview:_currentImgView];
-        
         _timeinterval = 2.0f;
+        _textColor = [UIColor blackColor];
+        _textFont = [UIFont systemFontOfSize:15.0f];
         
         UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(tapClick:)];
         [self.scrollView addGestureRecognizer:tap];
@@ -59,12 +61,47 @@
     return self;
 }
 
++ (instancetype)loopImageViewWithFrame:(CGRect)frame isHorizontal:(BOOL)isHorizontal
+{
+    FSLoopScrollView *loopView = [[self alloc] initWithFrame:frame];
+    loopView.lastImgView = [[UIImageView alloc]init];
+    [loopView.scrollView addSubview:loopView.lastImgView];
+    
+    loopView.nextImgView = [[UIImageView alloc]init];
+    [loopView.scrollView addSubview:loopView.nextImgView];
+    
+    loopView.currentImgView = [[UIImageView alloc]init];
+    [loopView.scrollView addSubview:loopView.currentImgView];
+    
+    loopView.isHorizontal = isHorizontal;
+    
+    return loopView;
+}
+
++ (instancetype)loopTitleViewWithFrame:(CGRect)frame isTitleView:(BOOL)isTitleView titleImgArr:(NSArray *)titleImgArr
+{
+    FSLoopScrollView *loopView = [[self alloc] initWithFrame:frame];
+    loopView.currentLabel = [[YYLabel alloc]init];
+    [loopView.scrollView addSubview:loopView.currentLabel];
+    
+    loopView.nextLabel = [[YYLabel alloc]init];
+    [loopView.scrollView addSubview:loopView.nextLabel];
+    
+    loopView.isTitleView = isTitleView;
+    loopView.titleImgArr = titleImgArr;
+    return loopView;
+}
+
 #pragma mark Action
 - (void)onTimer:(NSTimer *)timer
 {
-    [self.scrollView setContentOffset:CGPointMake(CGRectGetWidth(self.bounds)*2, 0) animated:YES];
+    CGPoint pointMake = _isHorizontal?CGPointMake(CGRectGetWidth(self.bounds)*2, 0):CGPointMake(0, CGRectGetHeight(self.bounds)*2);
+    if (_isTitleView) {
+        pointMake = CGPointMake(0, CGRectGetHeight(self.bounds));
+    }
+    [self.scrollView setContentOffset:pointMake animated:YES];
 }
-
+//scrollview的触摸手势
 - (void)tapClick:(UITapGestureRecognizer *)tap
 {
     if (self.tapClickBlock) {
@@ -72,6 +109,7 @@
     }
 }
 
+//根据index动态更新图片，通过偏移量控制currentImg一直显示在当前位置
 - (void)refreshCurrentImageView
 {
     NSInteger index = _currentIndex;
@@ -79,29 +117,46 @@
     [self dynamicLoadImageView:_currentImgView imageData:self.imgResourceArr[index]];
     
     index = _currentIndex-1<0?self.imgResourceArr.count-1:_currentIndex-1;
-    [self dynamicLoadImageView:_leftImgView imageData:self.imgResourceArr[index]];
+    [self dynamicLoadImageView:_lastImgView imageData:self.imgResourceArr[index]];
     
     index = _currentIndex+1>=self.imgResourceArr.count?0:_currentIndex+1;
-    [self dynamicLoadImageView:_rightImgView imageData:self.imgResourceArr[index]];
+    [self dynamicLoadImageView:_nextImgView imageData:self.imgResourceArr[index]];
     
-    [self.scrollView setContentOffset:CGPointMake(CGRectGetWidth(self.bounds), 0)];
+    CGPoint pointMake = _isHorizontal?CGPointMake(CGRectGetWidth(self.bounds), 0):CGPointMake(0, CGRectGetHeight(self.bounds));
+    [self.scrollView setContentOffset:pointMake];
 }
 
+//更新index
 - (void)refreshCurrentIndex
 {
-    if (self.scrollView.contentOffset.x >= CGRectGetWidth(self.bounds)*1.5) {
-        _currentIndex ++;
-        if (_currentIndex > self.imgResourceArr.count-1) {
-            _currentIndex = 0;
+    if (_isHorizontal) {
+        if (self.scrollView.contentOffset.x >= CGRectGetWidth(self.bounds)*1.5) {
+            _currentIndex ++;
+            if (_currentIndex > self.imgResourceArr.count-1) {
+                _currentIndex = 0;
+            }
+        }else if (self.scrollView.contentOffset.x < CGRectGetWidth(self.bounds)*0.5){
+            _currentIndex --;
+            if (_currentIndex < 0) {
+                _currentIndex = self.imgResourceArr.count-1;
+            }
         }
-    }else if (self.scrollView.contentOffset.x < CGRectGetWidth(self.bounds)*0.5){
-        _currentIndex --;
-        if (_currentIndex < 0) {
-            _currentIndex = self.imgResourceArr.count-1;
+    }else{
+        if (self.scrollView.contentOffset.y >= CGRectGetHeight(self.bounds)*1.5) {
+            _currentIndex ++;
+            if (_currentIndex > self.imgResourceArr.count-1) {
+                _currentIndex = 0;
+            }
+        }else if (self.scrollView.contentOffset.y < CGRectGetHeight(self.bounds)*0.5){
+            _currentIndex --;
+            if (_currentIndex < 0) {
+                _currentIndex = self.imgResourceArr.count-1;
+            }
         }
     }
+    
 }
-
+//加载图片，网络或本地图片
 - (void)dynamicLoadImageView:(UIImageView *)imageView imageData:(id)data
 {
     if ([data isKindOfClass:[UIImage class]]) {
@@ -113,6 +168,27 @@
             [imageView sd_setImageWithURL:[NSURL URLWithString:imageName] placeholderImage:nil];
         }else{
             imageView.image = [UIImage imageNamed:imageName];
+        }
+    }
+}
+
+- (void)refreshCurrentTitleView
+{
+    NSInteger index = _currentIndex;
+    self.currentLabel.attributedText = [self getAttachmentTextWithStr:self.titlesArr[index] image:self.titleImgArr[index]];
+    
+    index = _currentIndex+1>=self.titlesArr.count?0:_currentIndex+1;
+    self.nextLabel.attributedText = [self getAttachmentTextWithStr:self.titlesArr[index] image:self.titleImgArr[index]];
+    
+    self.scrollView.contentOffset = CGPointMake(0, 0);
+}
+
+- (void)refreshTitleIndex
+{
+    if (self.scrollView.contentOffset.y >= CGRectGetHeight(self.bounds)*0.5) {
+        _currentIndex ++;
+        if (_currentIndex > self.titlesArr.count-1) {
+            _currentIndex = 0;
         }
     }
 }
@@ -129,7 +205,6 @@
         _scrollView.scrollsToTop = NO;
         _scrollView.showsVerticalScrollIndicator = NO;
         _scrollView.showsHorizontalScrollIndicator = NO;
-        _scrollView.contentSize = CGSizeMake(CGRectGetWidth(self.bounds)*3, 0);
         [self addSubview:_scrollView];
     }
     return _scrollView;
@@ -162,40 +237,109 @@
     }
     
     if (imgResourceArr.count<=1) {
-        self.scrollView.contentSize = CGSizeMake(CGRectGetWidth(self.bounds), 0);
+        self.scrollView.contentSize = _isHorizontal?CGSizeMake(CGRectGetWidth(self.bounds), 0):CGSizeMake(0, CGRectGetHeight(self.bounds));
     }else{
-        self.scrollView.contentSize = CGSizeMake(CGRectGetWidth(self.bounds) *3, 0);
+        self.scrollView.contentSize = _isHorizontal?CGSizeMake(CGRectGetWidth(self.bounds)*3, 0):CGSizeMake(0, CGRectGetHeight(self.bounds)*3);
         [self.timer resumeTimerAfterTimeInterval:_timeinterval];
     }
     
     [self refreshCurrentImageView];
 }
 
+- (void)setTitlesArr:(NSArray *)titlesArr
+{
+    _titlesArr = titlesArr;
+    _currentIndex = 0;
+    
+    if (![_timer isValid]) {
+        _timer = [NSTimer scheduledTimerWithTimeInterval:_timeinterval target:self selector:@selector(onTimer:) userInfo:nil repeats:YES];
+        [_timer pauseTimer];
+    }
+    if (titlesArr.count<=1) {
+        self.scrollView.contentSize = CGSizeMake(0, CGRectGetHeight(self.bounds));
+    }else{
+        self.scrollView.contentSize = CGSizeMake(0, CGRectGetHeight(self.bounds)*2);
+    }
+    [self.timer resumeTimerAfterTimeInterval:_timeinterval];
+    [self refreshCurrentTitleView];
+}
+
+- (void)setTitleImgArr:(NSArray *)titleImgArr
+{
+    _titleImgArr = titleImgArr;
+}
+
+- (void)setTextFont:(UIFont *)textFont
+{
+    _textFont = textFont;
+}
+
+- (void)setTextColor:(UIColor *)textColor
+{
+    _textColor = textColor;
+}
+
 #pragma mark UIScrollViewDelegate
 
 - (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView
 {
+    //触摸滑动时暂停定时器
     [self.timer pauseTimer];
 }
 
 - (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView
 {
-    [self refreshCurrentIndex];
+    _isTitleView?[self refreshTitleIndex]:[self refreshCurrentIndex];
+    
     if (self.pageControl.currentPage != _currentIndex) {
         self.pageControl.currentPage = _currentIndex;
         [self refreshCurrentImageView];
     }
-    
+    //触摸滚动结束重启定时器
     [self.timer resumeTimerAfterTimeInterval:_timeinterval];
 }
 
 - (void)scrollViewDidEndScrollingAnimation:(UIScrollView *)scrollView
 {
-    [self refreshCurrentIndex];
-    if (self.pageControl.currentPage != _currentIndex) {
-        self.pageControl.currentPage = _currentIndex;
-        [self refreshCurrentImageView];
+    _isTitleView?[self refreshTitleIndex]:[self refreshCurrentIndex];
+    
+    if (_isTitleView) {
+        [self refreshCurrentTitleView];
+    }else{
+        if (self.pageControl.currentPage != _currentIndex) {
+            self.pageControl.currentPage = _currentIndex;
+            [self refreshCurrentImageView];
+        }
     }
 }
+
+#pragma mark private
+
+/**
+ 带图片头的文字循环
+
+ @param labelText label赋值
+ @param data 本地图片资源（可置空）
+ @return 拼接好的富文本
+ */
+- (NSMutableAttributedString *)getAttachmentTextWithStr:(NSString *)labelText image:(id)data
+{
+    UIImage *image;
+    if ([data isKindOfClass:[UIImage class]]) {
+        image = (UIImage *)data;
+    }else if ([data isKindOfClass:[NSString class]]){
+        NSString *imageName = (NSString *)data;
+        image = [UIImage imageNamed:imageName];
+    }
+    
+    NSMutableAttributedString *text = [NSMutableAttributedString new];
+    NSMutableAttributedString *attachText = [NSMutableAttributedString yy_attachmentStringWithContent:image contentMode:UIViewContentModeCenter attachmentSize:image.size alignToFont:self.textFont alignment:YYTextVerticalAlignmentCenter];
+    [text appendAttributedString:attachText];
+    [text appendAttributedString:[[NSAttributedString alloc] initWithString:[NSString stringWithFormat:@" %@",labelText] attributes:nil]];
+    text.yy_font = self.textFont;
+    text.yy_color = self.textColor;
+    return text;
+}
+
 
 @end
--- FSLoopScrollViewDemo/FSLoopScrollViewDemo/ViewController.m
+++ FSLoopScrollViewDemo/FSLoopScrollViewDemo/ViewController.m
@@ -9,7 +9,6 @@
 #import "ViewController.h"
 #import "FSLoopScrollView.h"
 
-//caonima
 
 @interface ViewController ()
 
@@ -21,7 +20,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     {
-        FSLoopScrollView *loopView = [[FSLoopScrollView alloc]initWithFrame:CGRectMake(10, 20, CGRectGetWidth(self.view.bounds)-20, 200)];
+        FSLoopScrollView *loopView = [FSLoopScrollView loopImageViewWithFrame:CGRectMake(10, 20, CGRectGetWidth(self.view.bounds)-20, 200) isHorizontal:NO];
         loopView.imgResourceArr = @[@"1.jpg",@"2.jpg",@"3.jpg",@"4.jpg",@"5.jpg"];
         loopView.tapClickBlock = ^(FSLoopScrollView *loopView){
             NSString *message = [NSString stringWithFormat:@"老%ld被点啦",(long)loopView.currentIndex+1];
@@ -31,12 +30,26 @@
         [self.view addSubview:loopView];
     }
     {
-        FSLoopScrollView *loopView = [[FSLoopScrollView alloc]initWithFrame:CGRectMake(10, 300, CGRectGetWidth(self.view.bounds)-20, 200)];
+        FSLoopScrollView *loopView = [FSLoopScrollView loopImageViewWithFrame:CGRectMake(10, 300, CGRectGetWidth(self.view.bounds)-20, 200) isHorizontal:YES];
         loopView.imgResourceArr = @[@"http://img05.tooopen.com/images/20150202/sy_80219211654.jpg",
                                     @"http://img06.tooopen.com/images/20161123/tooopen_sy_187628854311.jpg",
                                     @"http://img07.tooopen.com/images/20170306/tooopen_sy_200775896618.jpg",
                                     @"http://img06.tooopen.com/images/20170224/tooopen_sy_199503612842.jpg",
                                     @"http://img02.tooopen.com/images/20160316/tooopen_sy_156105468631.jpg"];
+        loopView.tapClickBlock = ^(FSLoopScrollView *loopView){
+            NSString *message = [NSString stringWithFormat:@"老%ld被点啦",(long)loopView.currentIndex+1];
+            UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"大顺啊" message:message delegate:self cancelButtonTitle:@"love you" otherButtonTitles:nil, nil];
+            [alert show];
+        };
+        [self.view addSubview:loopView];
+    }
+    {
+        FSLoopScrollView *loopView = [FSLoopScrollView loopTitleViewWithFrame:CGRectMake(10, 550, CGRectGetWidth(self.view.bounds)-20, 50) isTitleView:YES titleImgArr:@[@"home_ic_new",@"home_ic_hot",@"home_ic_new",@"home_ic_new",@"home_ic_hot"]];
+        loopView.titlesArr = @[@"这是一个简易的文字轮播",
+                               @"This is a simple text rotation",
+                               @"นี่คือการหมุนข้อความที่เรียบง่าย",
+                               @"Это простое вращение текста",
+                               @"이것은 간단한 텍스트 회전 인"];
         loopView.tapClickBlock = ^(FSLoopScrollView *loopView){
             NSString *message = [NSString stringWithFormat:@"老%ld被点啦",(long)loopView.currentIndex+1];
             UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"大顺啊" message:message delegate:self cancelButtonTitle:@"love you" otherButtonTitles:nil, nil];
